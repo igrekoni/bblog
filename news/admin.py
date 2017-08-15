271c4f84cc32bf71be1a7cb19f543e7eb70addd8
igrekoni <igrekoni@gmail.com>
2017-08-15 13:21:26 +0300

    single post model created

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import SingleNews
 
-# Register your models here.
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ["__unicode__", "updated", "timestamp"]
+    list_display_links = ["__unicode__"]
+    list_filter = ["updated", "timestamp"]
+    search_fields = ["__unicode__", "fullText"]
+
+    class Meta:
+        model = SingleNews
+
+
+admin.site.register(SingleNews, NewsAdmin)
--- news/models.py
+++ news/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class SingleNews(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
+    title = models.CharField(max_length=120, blank=False)
+    previewText = models.TextField(max_length=260, blank=True)
+    fullText = models.TextField()
+    publish = models.DateField(auto_now=False, auto_now_add=False)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+    slug = models.SlugField(unique=True)
+    category = models.CharField(max_length=40, default='Common')
+
+    def __unicode__(self):
+        return self.title
+
+    def __str__(self):
+        return self.title
