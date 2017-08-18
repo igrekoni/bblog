9d079c440b1f02408d6657038eb825e9e315f3d4
igrekoni <igrekoni@gmail.com>
2017-08-18 20:52:36 +0300

    single post model created

--- news/models.py
+++ news/models.py
@@ -1,5 +1,8 @@
 from django.conf import settings
 from django.db import models
+from django.db.models.signals import pre_save
+from unidecode import unidecode
+from django.utils.text import slugify
 
 
 class SingleNews(models.Model):
@@ -11,10 +14,45 @@ class SingleNews(models.Model):
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
     slug = models.SlugField(unique=True)
-    category = models.CharField(max_length=40, default='Common')
+    CH = u'Дети'
+    TH = u'Вещи'
+    DS = u'Досуг'
+    TR = u'Путешествия'
+    CATEGORY_CHOICES = (
+        (CH, u'Дети'),
+        (TH, u'Вещи'),
+        (DS, u'Досуг'),
+        (TR, u'Путешествия'),
+    )
+    category = models.CharField(max_length=40, choices=CATEGORY_CHOICES, default=CH)
 
     def __unicode__(self):
         return self.title
 
     def __str__(self):
         return self.title
+
+
+def create_slug(instance, new_slug=None):
+    slug = slugify(unidecode(instance.title))
+    if new_slug is not None:
+        slug = new_slug
+    qs = SingleNews.objects.filter(slug=slug).order_by("-id")
+    exists = qs.exists()
+    if exists:
+        new_slug = "%s-%s" % (slug, qs.first().id)
+        return create_slug(instance, new_slug=new_slug)
+    return slug
+
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+
+    # if instance.content:
+    #     html_string = instance.content
+    #     read_time_var = get_read_time(html_string)
+    #     instance.read_time = read_time_var
+
+
+pre_save.connect(pre_save_post_receiver, sender=SingleNews)
