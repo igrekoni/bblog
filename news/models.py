6e785575ba39e60baedf36bddd5783eab3e5edd1
igrekoni <igrekoni@gmail.com>
2017-08-27 02:42:18 +0300

    actual migrations to db

--- news/models.py
+++ news/models.py
@@ -13,6 +13,10 @@ class PostManager(models.Manager):
         return super(PostManager, self).filter(draft=False).filter(timezone)
 
 
+def upload_location(instance, filename):
+    return "%s/%s" % (instance.id, filename)
+
+
 class SingleNews(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
     title = models.CharField(max_length=120, blank=False)
@@ -22,6 +26,12 @@ class SingleNews(models.Model):
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
     slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to=upload_location,
+                              null=True, blank=True,
+                              height_field='height_field',
+                              width_field='width_field')
+    height_field = models.IntegerField(default=0)
+    width_field = models.IntegerField(default=0)
     CH = u'Дети'
     TH = u'Вещи'
     DS = u'Досуг'
