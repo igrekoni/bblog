2d430f2e191be3ae9bce3997ae2c68d23c53261a
igrekoni <igrekoni@gmail.com>
2017-08-27 03:08:51 +0300

    static folder added

--- bblog/urls.py
+++ bblog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from django.contrib import admin
 from news.views import (
@@ -16,3 +18,8 @@ urlpatterns = [
     url(r'^travel/$', TravelListView.as_view()),
     url(r'^$', MainListView.as_view()),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
