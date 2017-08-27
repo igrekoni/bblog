d52d38cc767de20bc98376c6a15ad46e3574f5df
igrekoni <igrekoni@gmail.com>
2017-08-27 13:54:25 +0300

    static and scripts/css configured to local use

--- bblog/settings/local.py
+++ bblog/settings/local.py
@@ -107,8 +107,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, '/static_cdn/')#'/static_cdn/'
+MEDIA_URL = os.path.join(BASE_DIR, '/media_cdn/')#"/media_cdn/"
 
-MEDIA_URL = "/media/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
+#STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_cdn")
+]
